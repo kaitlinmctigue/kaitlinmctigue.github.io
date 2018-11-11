59c3a53238084d78a93cc44f6da5b153398846f1
Katie McTigue <kaitlinmctigue@gmail.com>
2018-11-11 13:29:40 -0500

    Darkmode and work toggle

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,49 @@ import Contact from './Contact';
 import Resume from './Resume';
 import Portfolio from './Portfolio';
 
+
+
 export default class App extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dark: false,
+            work: true
+        };
+
+        this.work = [
+            {
+                'id': 0,
+                'title': 'Diverse Computing Rebrand',
+                'tags': [],
+                'img': ''
+            }
+        ];
+
+        this.changeTheme = this.changeTheme.bind(this);
+        this.workOrPlay = this.workOrPlay.bind(this);
+    }
+
+    changeTheme() {
+        this.setState({ dark: !this.state.dark });
+    }
+
+    workOrPlay() {
+        this.setState({ work: !this.state.work });
     }
 
     render() {
         return (
-            <div className='theme-light'>
+            <div className={(this.state.dark ? 'theme-dark' : 'theme-light')}>
                 <div className='base'>
                     <Router>
                         <div>
                             <div className='header d-flex flex-column flex-sm-row'>
                                 <ul className='header__utility col'>
                                     <li>
-                                        <a className="theme-toggle link">Toggle Colors</a>
+                                        <a className="theme-toggle link" onClick={this.changeTheme}>Toggle Colors</a>
                                     </li>
                                 </ul>
                                 <ul className='header__links col'>
