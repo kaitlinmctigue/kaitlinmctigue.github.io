6f0e6462c3f186d2c250dd7238bd3e3cb4de703b
Katie McTigue <kaitlinmctigue@gmail.com>
2018-11-28 22:29:29 -0500

    Responsive styling.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import resume from '../mctigue_resume.pdf';
 
+const resumelink = '../images/mctigue_resume.pdf';
+const linkedin = 'https://www.linkedin.com/in/katiemctigue';
+
 export default class Resume extends Component {
 
     constructor() {
@@ -11,15 +14,27 @@ export default class Resume extends Component {
 
     render() {
         return (
-            <div>
+            <div className={'resume'}>
                 <Document
                     file={resume}>
                     <Page width={1100} pageNumber={1} />
                 </Document>
-                <Link to={resume} className={'link'}>
-                    <i className='fas fa-arrow-circle-down'></i>
-                    Download PDF
-                </Link>
+                <div className={'resume__actions'}>
+                    <div className={'row'}>
+                        <div className={'col-xs-12 col-md-6'}>
+                            <Link to={resumelink} target={'_blank'} className={'link'}>
+                                <span className='fas fa-arrow-circle-down'></span>
+                                Download PDF Resume
+                            </Link>
+                        </div>
+                        <div className={'col-xs-12 col-md-6'}>
+                            <Link to={linkedin} className={'link'}>
+                                <span className='fas fa-arrow-circle-down'></span>
+                                View My LinkedIn Profile
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
