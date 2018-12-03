fbefafc7461cf5050633f9d8f54b6a9368583b27
Katie McTigue <kaitlinmctigue@gmail.com>
2018-12-02 22:28:35 -0500

    trying to fix routing issue

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import Resume from './Resume';
 import Portfolio from './Portfolio';
 import DciBrand from './portfolio/items/DciBrand';
 import Environmental from './portfolio/items/Environmental';
+import ScrollToTop from './ScrollToTop';
 
 const portfolioItems= [
     {
@@ -44,55 +45,57 @@ export default class App extends Component {
         return (
             <div className={'theme ' + (this.state.dark ? 'theme-dark' : 'theme-light')}>
                 <div className='base'>
-                    <Router>
-                        <div>
-                            <Navbar collapseOnSelect className='header'>
-                                <Link to='/' className="logo"></Link>
-                                <Navbar.Header>
-                                    <Navbar.Toggle />
-                                </Navbar.Header>
-                                <Navbar.Collapse>
-                                    <Nav pullRight className='header__utility'>
-                                        <NavItem className="theme-toggle link" onClick={this.changeTheme}>
-                                            <FontAwesomeIcon icon={this.state.dark ? 'sun' : 'moon'}/>
-                                        </NavItem>
-                                    </Nav>
-                                    <div className='header__links'>
-                                        <Nav>
-                                            <LinkContainer to='/' exact>
-                                                <NavItem eventKey={1} className='link'>
-                                                    Portfolio
-                                                </NavItem>
-                                            </LinkContainer>
-                                            <LinkContainer to={'/resume'}>
-                                                <NavItem eventKey={2} className='link'>
-                                                    Resume
-                                                </NavItem>
-                                            </LinkContainer>
-                                            <LinkContainer to="/contact">
-                                                <NavItem eventKey={3} className='link'>
-                                                    Contact
-                                                </NavItem>
-                                            </LinkContainer>
+                    <Router onUpdate={() => window.scrollTo(0, 0)}>
+                        <ScrollToTop>
+                            <div>
+                                <Navbar collapseOnSelect className='header'>
+                                    <Link to='/' className="logo"></Link>
+                                    <Navbar.Header>
+                                        <Navbar.Toggle />
+                                    </Navbar.Header>
+                                    <Navbar.Collapse>
+                                        <Nav pullRight className='header__utility'>
+                                            <NavItem className="theme-toggle link" onClick={this.changeTheme}>
+                                                <FontAwesomeIcon icon={this.state.dark ? 'sun' : 'moon'}/>
+                                            </NavItem>
                                         </Nav>
-                                    </div>
-                                </Navbar.Collapse>
-                            </Navbar>
+                                        <div className='header__links'>
+                                            <Nav>
+                                                <LinkContainer to='/' exact>
+                                                    <NavItem eventKey={1} className='link'>
+                                                        Portfolio
+                                                    </NavItem>
+                                                </LinkContainer>
+                                                <LinkContainer to={'/resume'}>
+                                                    <NavItem eventKey={2} className='link'>
+                                                        Resume
+                                                    </NavItem>
+                                                </LinkContainer>
+                                                <LinkContainer to="/contact">
+                                                    <NavItem eventKey={3} className='link'>
+                                                        Contact
+                                                    </NavItem>
+                                                </LinkContainer>
+                                            </Nav>
+                                        </div>
+                                    </Navbar.Collapse>
+                                </Navbar>
 
-                            <div className="body text-center">
+                                <div className="body text-center">
 
-                                {/*main*/}
-                                <Route exact path="/" component={Portfolio} />
-                                <Route path="/home" component={Portfolio} routes={portfolioItems} />
-                                <Route path="/resume" component={Resume} />
-                                <Route path="/contact" component={Contact} />
+                                    {/*main*/}
+                                    <Route exact path="/" component={Portfolio} />
+                                    <Route path="/home" component={Portfolio} routes={portfolioItems} />
+                                    <Route path="/resume" component={Resume} />
+                                    <Route path="/contact" component={Contact} />
 
-                                {/*portfolio*/}
-                                {portfolioItems.map(item => (
-                                    <Route key={item.path} path={'/portfolio/' + item.path} component={item.comp} />
-                                ))}
+                                    {/*portfolio*/}
+                                    {portfolioItems.map(item => (
+                                        <Route key={item.path} path={'/portfolio/' + item.path} component={item.comp} />
+                                    ))}
+                                </div>
                             </div>
-                        </div>
+                        </ScrollToTop>
                     </Router>
                 </div>
             </div>
---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class ScrollToTop extends Component {
+    componentDidUpdate() {
+        window.scrollTo(0, 0);
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
+export default withRouter(ScrollToTop);
+
+ScrollToTop.propTypes = {
+    location: PropTypes.array,
+    children: PropTypes.array
+};
