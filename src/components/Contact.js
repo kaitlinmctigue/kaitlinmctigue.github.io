2e4ed531353ba51ddd5101b9273f4bd1f512afd5
Katie McTigue <kaitlinmctigue@gmail.com>
2018-12-28 23:48:17 -0500

    contact page tweaks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,13 +21,26 @@ export default class Contact extends Component {
                     </h1>
                     <div className={'contact__text__body'}>
                         <p>kaitlinmctigue at gmail dot com</p>
-                        <p><a className={'link'} href={'https://www.linkedin.com/in/katiemctigue'} target={'_blank'}>linkedin.com/in/katiemctigue</a></p>
-                        <SocialIcon url="https://www.linkedin.com/in/katiemctigue" />
-                        <SocialIcon url="https://github.com/kaitlinmctigue" />
-                        <SocialIcon url="https://medium.com/@katiemctigue" />
-                        <SocialIcon url="https://twitter.com/MctigueKatie?lang=en" />
-                        <SocialIcon url="https://www.instagram.com/katiemctigue/" />
-                        <SocialIcon url="https://open.spotify.com/user/kaitlinmctigue" />
+                        {/*<p><a className={'link'} href={'https://www.linkedin.com/in/katiemctigue'} target={'_blank'}>linkedin.com/in/katiemctigue</a></p>*/}
+
+                        <div className={'contact__text__body__social'}>
+                            <div className={'row'}>
+                                <div className={'col-xs-12 contact__text__body__social--top'}>
+                                    I actively do stuff on <br/>
+                                    <SocialIcon url="https://www.linkedin.com/in/katiemctigue" />
+                                    <SocialIcon url="https://medium.com/@katiemctigue" />
+                                    <SocialIcon url="https://github.com/kaitlinmctigue" />
+                                    <SocialIcon url="https://www.instagram.com/katiemctigue/" />
+                                </div>
+
+                                <div className={'col-xs-12 contact__text__body__social--bottom'}>
+                                    Maybe one day I will update my <br/>
+                                    <SocialIcon url="https://twitter.com/MctigueKatie?lang=en" />
+                                    <SocialIcon url="https://dribbble.com/katiemctigue" />
+                                    <SocialIcon url="https://open.spotify.com/user/kaitlinmctigue" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
